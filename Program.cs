60e7156eef2cd248c929e93eef5ed45501ff4d09
ThibautJoncour <96741150+ThibautJoncour@users.noreply.github.com>
2025-02-10 12:07:43 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,14 +1,30 @@
 using MagicNumber.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<CustomAuthenticationStateProvider>();
+
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddSingleton<DatabaseService>();
+builder.Services.AddScoped<ProtectedSessionStorage>();
+
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+builder.Services.AddAuthorizationCore();
+// Enregistrement du provider d'authentification personnalisé
+builder.Services.AddScoped<CustomAuthenticationStateProvider>();
+
+
+builder.Services.AddAuthorizationCore();
 
 
 var app = builder.Build();
@@ -17,13 +33,10 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-
 app.UseRouting();
 
 app.MapBlazorHub();
